0a894fd12d97d44ec6a8daf336ac1b1280be43a3
mmasek <mmasek@HD71921025>
2022-03-21 16:28:27 +0800

    Initial comit.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello world!\n");
+            Console.Write("Hello world!!!\n");
         }
     }
 }
